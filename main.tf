c0fd6e658e8fa6b680826ad33e95a9c21aaf49cb
Olivier Hoareau <oha@greenberets.io>
2022-05-17 15:47:56 +0200

    chore: add terraform.tf and constraints to AWS > 4.0.0

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "website" {
   source         = "genstackio/website/aws"
-  version        = "0.2.1"
+  version        = "0.3.2"
   name           = var.name
   bucket_name    = var.bucket_name
   zone           = var.dns_zone
