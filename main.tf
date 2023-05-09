431be1ad36a32de3392437c45747449bf040239d
Olivier Hoareau <oha@greenberets.io>
2023-05-09 19:13:24 +0200

    fix: fix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "website" {
   source         = "genstackio/website/aws"
-  version        = "0.3.12"
+  version        = "0.3.13"
   name           = var.name
   bucket_name    = var.bucket_name
   zone           = var.dns_zone
